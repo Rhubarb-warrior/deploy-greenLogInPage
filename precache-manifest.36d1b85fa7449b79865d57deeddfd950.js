f72dbbde7e54b12b15327470200020605ddbd678
Rhubarb-warrior <65822593+Rhubarb-warrior@users.noreply.github.com>
2020-09-17 00:47:01 +0100

    Updates

---
+++ precache-manifest.36d1b85fa7449b79865d57deeddfd950.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8261ed56581c5750c381957c1c6c6198",
+    "url": "/deploy-greenLogInPage/index.html"
+  },
+  {
+    "revision": "deb6317a41e3111479c5",
+    "url": "/deploy-greenLogInPage/static/css/main.7f90e3a3.chunk.css"
+  },
+  {
+    "revision": "bab9311a5f344513cfed",
+    "url": "/deploy-greenLogInPage/static/js/2.224513ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-greenLogInPage/static/js/2.224513ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deb6317a41e3111479c5",
+    "url": "/deploy-greenLogInPage/static/js/main.c22a9d2e.chunk.js"
+  },
+  {
+    "revision": "107660e05411a2d0276b",
+    "url": "/deploy-greenLogInPage/static/js/runtime-main.11a49b1a.js"
+  },
+  {
+    "revision": "9da97edd306f798b1cb2eb6dca334e22",
+    "url": "/deploy-greenLogInPage/static/media/logo-r.9da97edd.svg"
+  }
+]);
